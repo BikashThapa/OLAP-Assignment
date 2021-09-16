aeea85b1211f432e2ea264c83384b743f32d21c6
Bikash <bthapa489@gmail.com>
2021-09-16 06:28:30 +0545

    master: sql updated

--- ETL-Project/schema/create_raw_dim_date.sql
+++ ETL-Project/schema/create_raw_dim_date.sql
@@ -1,10 +1,10 @@
 CREATE TABLE dim_date(
-					date_id SERIAL,
-					punch_in_time TIMESTAMP,
-					punch_out_time TIMESTAMP,
-					punch_apply_date DATE NOT NULL,
-					hire_date DATE NOT NULL,
-					terminated_date DATE NOT NULL,
-					dob DATE NOT NULL,
-					CONSTRAINT pk_date_id PRIMARY KEY(date_id)
+date_id SERIAL,
+punch_in_time TIMESTAMP,
+punch_out_time TIMESTAMP,
+punch_apply_date DATE NOT NULL,
+hire_date DATE NOT NULL,
+terminated_date DATE NOT NULL,
+dob DATE NOT NULL,
+CONSTRAINT pk_date_id PRIMARY KEY(date_id)
 );
--- ETL-Project/schema/create_raw_dim_employee.sql
+++ ETL-Project/schema/create_raw_dim_employee.sql
@@ -1,12 +1,12 @@
 CREATE TABLE dim_employee (
-                            employee_id SERIAL ,
-                            first_name VARCHAR(30) NOT NULL,
-                            last_name VARCHAR(30) NOT NULL,
-                            location VARCHAR(15) NOT NULL,
-                            employee_role VARCHAR(50),
-                            manager_role_id NUMERIC(7),
-                            cost_center NUMERIC (5) NOT NULL,
-                            paycode VARCHAR(7) NOT NULL,
-                            CONSTRAINT pk_employee_id PRIMARY KEY(employee_id),
+employee_id SERIAL ,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+location VARCHAR(15) NOT NULL,
+employee_role VARCHAR(50),
+manager_role_id NUMERIC(7),
+cost_center NUMERIC (5) NOT NULL,
+paycode VARCHAR(7) NOT NULL,
+CONSTRAINT pk_employee_id PRIMARY KEY(employee_id),
                             
 );
